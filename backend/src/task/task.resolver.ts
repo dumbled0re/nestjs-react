89319109cc3d5d5e5f10b7868b6b358a8a367f01
dumbled0re <smu315064@gmail.com>
2023-09-19 15:03:40 +0900

    feat: タスクリゾルバーの追加

---
+++ backend/src/task/task.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { Task } from './models/task.model';
+import { TaskService } from './task.service';
+
+@Resolver()
+export class TaskResolver {
+  // DI（Dependency Injection）を使ってTaskServiceを注入する
+  constructor(private readonly taskService: TaskService) {}
+
+  // GraphQLのクエリを定義する
+  @Query(() => [Task], { nullable: 'items' })
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+}
