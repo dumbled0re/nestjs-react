bcd1353c5a114cc505f5c18a79772eed5a90e1d5
dumbled0re <smu315064@gmail.com>
2023-09-29 20:58:28 +0900

    feat: nestjsからデータベースを操作できるようにprismaのクライアントをセットアップ

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  // データベースの接続を確立する
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  // データベースの接続を切断する
+  // async enableShutdownHooks(app: INestApplication) {
+  //   this.$on('beforeExit', async () => {
+  //     await app.close();
+  //   });
+  // }
+}
